3c4114ded9f2c060371c7e4e3050c2751fa2ceba
Tony DiCola <tony@tonydicola.com>
2018-03-30 11:31:14 -0700

    Add redis-backed message bus implementation and test.

--- smart_instant_pot/smart_instant_pot/services/message_bus.py
+++ smart_instant_pot/smart_instant_pot/services/message_bus.py
@@ -4,6 +4,8 @@
 # common message bus broker.
 # Author: Tony DiCola
 import abc
+import threading
+import time
 
 import redis
 
@@ -26,8 +28,8 @@ class MessageBus(abc.ABC):
     def subscribe(self, channel, callback):
         """Subscribe for notification of new messages on the specified
         channel.  Provide a callback function which will be invoked with
-        two parameters, the channel name (as a string) and the message data
-        (as a byte string).
+        two parameters, the channel name and the message data (both as byte
+        strings).
         """
         pass
 
@@ -57,7 +59,7 @@ class SimpleMessageBus(MessageBus):
         message = to_bytes(message)
         if channel in self._bus:
             for cb in self._bus[channel]:
-                cb(channel, message)
+                cb(to_bytes(channel), message)
 
     def subscribe(self, channel, callback):
         # Add this callback to the list of callbacks for this channel if it
@@ -68,3 +70,83 @@ class SimpleMessageBus(MessageBus):
         # Remove the specified callback if it's registered with the channel.
         if channel in self._bus:
             self._bus[channel].discard(callback)
+
+
+class RedisThreadedMessageBus(MessageBus):
+    """Redis-backed message bus implementation using a thread to process and
+    deliver messages in the background.  Uses a redis server and its pubsub
+    support to send and receive messages.  You must pass in any redis-py
+    StrictRedis keyword arguments like host and port to initialize the instance.
+    You can optionally specify a namespace that will be appended to channel
+    names to help isolated them from other data in the redis server.
+    """
+
+    def __init__(self, namespace=None, **kwarg):
+        self._namespace = namespace
+        self._callbacks = {}
+        self._callbacks_lock = threading.Lock()
+        self._redis = redis.StrictRedis(**kwarg)
+        # Setup pubsub connection and subscribe to all channels in the namespace
+        # for this instance.  We do this because redis-py doesn't handle adding
+        # new subscriptions well (its sockets are blocking and the threading
+        # model has issues too), so we just subscribe to everything and filter
+        # out channels we care about when messages are received.
+        self._pubsub = self._redis.pubsub(ignore_subscribe_messages=True)
+        sub_channel = self._channel('*', as_bytes=False)
+        self._pubsub.psubscribe(**{sub_channel: self._callback})
+        self._thread = self._pubsub.run_in_thread(sleep_time=0.001)
+
+    def _callback(self, data):
+        # Main callback that will be invoked by redis-py when a message is
+        # received.  This grabs the channel and message, then looks for any
+        # subscribed callback functions and calls them.  Note that this will
+        # run in a background thread so it needs to be careful to syncronize
+        # access to data structures like the callback dictionary.
+        if data is not None and 'channel' in data and 'data' in data:
+            channel = data['channel']
+            message = data['data']
+            with self._callbacks_lock:
+                if channel in self._callbacks:
+                    for cb in self._callbacks[channel]:
+                        cb(channel, message)
+
+    def _channel(self, channel, as_bytes=True):
+        # Construct a channel name using any specified namespace.
+        if self._namespace is not None:
+            channel = build_id(self._namespace, channel)
+        if as_bytes:
+            return to_bytes(channel)
+        return channel
+
+    def deinit(self):
+        """Unregister all callbacks and close connections with the redis server.
+        This is useful when finished with the message bus and you want to
+        ensure no future message callbacks are fired.
+        """
+        if self._thread is not None:
+            self._thread.stop()
+            self._thread = None
+        with self._callbacks_lock:
+            if self._callbacks is not None:
+                self._callbacks = None
+
+    def __del__(self):
+        self.deinit()
+
+    def publish(self, channel, message):
+        channel = self._channel(channel)
+        self._redis.publish(channel, message)
+
+    def subscribe(self, channel, callback):
+        # Add this callback to the list of callbacks for this channel if it
+        # isn't already there.
+        channel = self._channel(channel)
+        with self._callbacks_lock:
+            self._callbacks.setdefault(channel, set()).add(callback)
+
+    def unsubscribe(self, channel, callback):
+        # Remove the specified callback if it's registered with the channel.
+        channel = self._channel(channel)
+        with self._callbacks_lock:
+            if channel in self._callbacks:
+                self._callbacks[channel].discard(callback)
--- smart_instant_pot/tests/test_message_bus.py
+++ smart_instant_pot/tests/test_message_bus.py
@@ -1,8 +1,9 @@
 # Smart Instant Pot Message Bus Tests
 # Author: Tony DiCola
+import time
 import unittest
 
-from smart_instant_pot.services.message_bus import SimpleMessageBus
+from smart_instant_pot.services.message_bus import SimpleMessageBus, RedisThreadedMessageBus
 
 
 class TestSimpleMessageBus(unittest.TestCase):
@@ -16,7 +17,7 @@ class TestSimpleMessageBus(unittest.TestCase):
         bus.subscribe('foo', callback)
         bus.publish('foo', 'bar')
         self.assertEqual(len(callback_calls), 1)
-        self.assertDictEqual(callback_calls[0], {'channel': 'foo',
+        self.assertDictEqual(callback_calls[0], {'channel': b'foo',
                                                  'message': b'bar'})
 
     def test_subscribe_and_publish_multi_subscriber(self):
@@ -32,10 +33,10 @@ class TestSimpleMessageBus(unittest.TestCase):
         bus.subscribe('foo', callback2)
         bus.publish('foo', 'bar')
         self.assertEqual(len(callback1_calls), 1)
-        self.assertDictEqual(callback1_calls[0], {'channel': 'foo',
+        self.assertDictEqual(callback1_calls[0], {'channel': b'foo',
                                                   'message': b'bar'})
         self.assertEqual(len(callback2_calls), 1)
-        self.assertDictEqual(callback2_calls[0], {'channel': 'foo',
+        self.assertDictEqual(callback2_calls[0], {'channel': b'foo',
                                                   'message': b'bar'})
 
     def test_subscribe_then_unsubscribe_and_publish(self):
@@ -49,14 +50,87 @@ class TestSimpleMessageBus(unittest.TestCase):
         bus.unsubscribe('foo', callback)
         bus.publish('foo', 'baz')
         self.assertEqual(len(callback_calls), 1)
-        self.assertDictEqual(callback_calls[0], {'channel': 'foo',
+        self.assertDictEqual(callback_calls[0], {'channel': b'foo',
                                                  'message': b'bar'})
 
     def test_publish_no_subscribers_does_nothing(self):
+        # Test publish does nothing when no subscribers are on the channel.
         bus = SimpleMessageBus()
         callback_calls = []
         def callback(channel, message):
             callback_calls.append({'channel': channel, 'message': message})
         bus.subscribe('foo', callback)
         bus.publish('bar', 'bar')
+        self.assertEqual(len(callback_calls), 0)
+
+
+@unittest.skip("Redis message bus integration tests must be manually run with redis.")
+class TestRedisThreadedMessageBus(unittest.TestCase):
+
+    REDIS_HOST = 'redis'
+
+    DELIVER_DELAY = 2.0  # Time to wait for messages to be delivered.
+                         # Simple but not ideal (consider polling for expected
+                         # state).
+
+    def setUp(self):
+        self._bus = RedisThreadedMessageBus(host=self.REDIS_HOST)
+
+    def tearDown(self):
+        self._bus.deinit()
+        self._bus = None
+
+    def test_subscribe_and_publish_one_subscriber(self):
+        # Test basic subscribe and publish to a channel works.
+        callback_calls = []
+        def callback(channel, message):
+            callback_calls.append({'channel': channel, 'message': message})
+        self._bus.subscribe('foo', callback)
+        self._bus.publish('foo', 'bar')
+        time.sleep(self.DELIVER_DELAY)
+        self.assertEqual(len(callback_calls), 1)
+        self.assertDictEqual(callback_calls[0], {'channel': b'foo',
+                                                 'message': b'bar'})
+
+    def test_subscribe_and_publish_multi_subscriber(self):
+        # Test multiple subscribers receive the same published message.
+        callback1_calls = []
+        def callback1(channel, message):
+            callback1_calls.append({'channel': channel, 'message': message})
+        callback2_calls = []
+        def callback2(channel, message):
+            callback2_calls.append({'channel': channel, 'message': message})
+        self._bus.subscribe('foo', callback1)
+        self._bus.subscribe('foo', callback2)
+        self._bus.publish('foo', 'bar')
+        time.sleep(self.DELIVER_DELAY)
+        self.assertEqual(len(callback1_calls), 1)
+        self.assertDictEqual(callback1_calls[0], {'channel': b'foo',
+                                                  'message': b'bar'})
+        self.assertEqual(len(callback2_calls), 1)
+        self.assertDictEqual(callback2_calls[0], {'channel': b'foo',
+                                                  'message': b'bar'})
+
+    def test_subscribe_then_unsubscribe_and_publish(self):
+        # Test unsubscribe removes a subscriber from future notifications.
+        callback_calls = []
+        def callback(channel, message):
+            callback_calls.append({'channel': channel, 'message': message})
+        self._bus.subscribe('foo', callback)
+        self._bus.publish('foo', 'bar')
+        self._bus.unsubscribe('foo', callback)
+        self._bus.publish('foo', 'baz')
+        time.sleep(self.DELIVER_DELAY)
+        self.assertEqual(len(callback_calls), 1)
+        self.assertDictEqual(callback_calls[0], {'channel': b'foo',
+                                                 'message': b'bar'})
+
+    def test_publish_no_subscribers_does_nothing(self):
+        # Test publish does nothing when no subscribers are on the channel.
+        callback_calls = []
+        def callback(channel, message):
+            callback_calls.append({'channel': channel, 'message': message})
+        self._bus.subscribe('foo', callback)
+        self._bus.publish('bar', 'bar')
+        time.sleep(self.DELIVER_DELAY)
         self.assertEqual(len(callback_calls), 0)
